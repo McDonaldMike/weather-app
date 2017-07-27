343915f2c5f3a67c7fc47edd06a33947bd203f02
Quinn Langille <quinnlangille@Quinns-MacBook-Pro.local>
2017-07-27 16:26:50 -0400

    added JS logic

--- js/weather-app.js
+++ js/weather-app.js
@@ -1,72 +1,77 @@
-(function(){
-var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
-var DARKSKY_API_KEY = '68ae7d38fb51db27bc7d79139fe20b2a';
-var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+(function() {
+	var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
+	var DARKSKY_API_KEY = '68ae7d38fb51db27bc7d79139fe20b2a';
+	var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
-var GOOGLE_MAPS_API_KEY = 'AIzaSyApuWuU8zo2iT8ExAHbGRJU_1pQkJBt71Q';
-var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+	var GOOGLE_MAPS_API_KEY = 'AIzaSyApuWuU8zo2iT8ExAHbGRJU_1pQkJBt71Q';
+	var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
-// This function returns a promise that will resolve with an object of lat/lng coordinates
-function getCoordinatesForCity(cityName) {
-  var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
+	// This function returns a promise that will resolve with an object of lat/lng coordinates
+	function getCoordinatesForCity(cityName) {
+		var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
 
-  return (
-    fetch(url) // Returns a promise for a Response
-    .then(response => response.json()) // Returns a promise for the parsed JSON
-    .then(data => data.results[0].geometry.location) // Transform the response to only take what we need
-  );
-}
+		return (fetch(url). // Returns a promise for a Response
+				then(response => response.json()). // Returns a promise for the parsed JSON
+				then(data => data.results[0].geometry.location) // Transform the response to only take what we need
+		);
+	}
 
-function getCurrentWeather(coords) {
-  var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
+	function getCurrentWeather(coords) {
+		var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
+		console.log(coords)
+		return (fetch(url).then(response => response.json()).then(data => data.currently));
+	}
 
-  return (
-    fetch(url)
-    .then(response => response.json())
-    .then(data => data.currently)
-  );
-}
+	var app = document.querySelector('#app');
+	var cityForm = app.querySelector('.city-form');
+	var cityInput = cityForm.querySelector('.city-input');
+	var cityWeather = app.querySelector('.city-weather');
+	var temp = document.querySelector('.temp');
+	var clouds = app.querySelector('.clouds');
 
-var app = document.querySelector('#app');
-var cityForm = app.querySelector('.city-form');
-var cityInput = cityForm.querySelector('.city-input');
-var cityWeather = app.querySelector('.city-weather');
-var temp = document.querySelector('.temp');
+	cityForm.addEventListener('click', function(event) {
+		event.preventDefault(); // prevent the form from submitting
 
-cityForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // prevent the form from submitting
+		var city = cityInput.value;
+		cityWeather.innerText = "Checking Weather"
 
+		getCoordinatesForCity(city).then(getCurrentWeather).then(function(weather) {
 
-  var city = cityInput.value;
-  cityWeather.innerText = "Checking Weather"
-  getCoordinatesForCity(city)
-  .then(getCurrentWeather)
-  .then(function(weather) {
-    console.log(weather)
-    console.log(typeof weather.temperature)
-    if(weather.temperature >= 30){
-    	temp.innerText = `It's really hot.`
-  	}
-    else if(weather.temperature<29 && weather.temperature>=25){
-    	temp.innerText = `It's hot.`
-    }
-    else if(weather.temperature<25 && weather.temperature>=20){
-    	temp.innerText = `It's kind of hot.`
-    }
-    else if(weather.temperature<20 && weather.temperature>=15){
-    	temp.innerText = `It's warm.`
-    }
-    else if(weather.temperature<15 && weather.temperature>=10){
-    	temp.innerText = `It's a little chilly.`
-    }
-    else if(weather.temperature<10 && weather.temperature>=0){
-    	temp.innerText = `It's cold.`
-    }
-    else if(weather.temperature<0){
-    	temp.innerText = `It's cold.`
-    }
+			// cityWeather.innerText = "Guess what? ";
+			cityWeather.innerText = "";
 
-	cityWeather.innerText = "hello";
-  });
-});
+			//Temperature
+			if(weather.temperature >= 41) {
+				cityWeather.innerText += `For the love of God stay inside `;
+			}else if (weather.temperature > 30 && weather.temperature <= 40) {
+				cityWeather.innerText += `It's really hot `;
+			} else if (weather.temperature < 29 && weather.temperature >= 25) {
+				cityWeather.innerText += `It's hot `;
+			} else if (weather.temperature < 25 && weather.temperature >= 20) {
+				cityWeather.innerText += `It's kind of hot `;
+			} else if (weather.temperature < 20 && weather.temperature >= 15) {
+				cityWeather.innerText += `It's warm `;
+			} else if (weather.temperature < 15 && weather.temperature >= 10) {
+				cityWeather.innerText += `It's a little chilly, you probably want a jacket `;
+			} else if (weather.temperature < 10 && weather.temperature >= 0) {
+				cityWeather.innerText += `It's cold `;
+			} else if (weather.temperature < 0) {
+				cityWeather.innerText += `It's literally freezing `;
+			}
+			//Humidity
+			//Summary
+			if (weather.summary === "Clear" || weather.summary === "Partly Cloudy" || weather.summary === "Mostly Cloudy") {
+				cityWeather.innerText += ` and the skies are ` + weather.summary.toLowerCase()
+			} else if (weather.summary === "Rain" || weather.summary === "Snow") {
+				cityWeather.innerText += ` and it's ` + weather.summary.toLowerCase() + "ing";
+			} else if (weather.summary === "Overcast" || weather.summary === "Dry") {
+				cityWeather.innerText += ` and it's ` + weather.summary.toLowerCase();
+			} else if (weather.summary === "Drizzle") {
+				cityWeather.innerText += ` and it's ` + weather.summary.toLowerCase() + "y";
+			} else if (weather.summary === "Light Rain") {
+				cityWeather.innerText += ` and it's raining a little.`;
+			}
+
+		});
+	});
 })(); //IFFE
